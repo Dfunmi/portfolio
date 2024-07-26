dfc598b369133b1efd43c97e027de56d5090a811
Oluwafunmilayo Owolabi <damilolaowolabi55@gmail.com>
2024-07-26 07:35:05 +0100

    Update Project.jsx

--- src/Components/Projects/Project.jsx
+++ src/Components/Projects/Project.jsx
@@ -1,6 +1,5 @@
 import styles from './ProjectStyles.module.css'
 import logo from '../../assets/logo - Copy.png'
-import portfolio from '../../assets/portfolio.png'
 import ProjectCard from '../Common/ProjectCard'
 
 function Project() {
@@ -9,7 +8,7 @@ function Project() {
       <h1 className='sectionTitle'>Projects</h1>
          <div className={styles.projectsContainer}>
       <ProjectCard src={logo} link="https://github.com/Dfunmi/job-listings-react" h3='Job Listings Site'/>
-       <ProjectCard src={portfolio} link="https://github.com/Dfunmi/job-listings-react" h3='Portfolio Project'/>
+       <ProjectCard src={logo} link="https://github.com/Dfunmi/job-listings-react" h3='Portfolio Project'/>
       {/* <ProjectCard src={logo} link="https://github.com/Dfunmi/job-listings-react" h3='Placeholder' p='Placeholder Project2'/>
       <ProjectCard src={logo} link="https://github.com/Dfunmi/job-listings-react" h3='Placeholder' p='placeholder Project3'/>  */}
       </div>
